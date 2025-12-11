b6e4b32fa8fc72cd87d1ec8ef1322229e9604d69
Zack Yuan <zack6yuan@gmail.com>
2025-12-11 10:05:07 -0600

    working on the profile page.

--- diinker-mobile/app/(tabs)/profile.tsx
+++ diinker-mobile/app/(tabs)/profile.tsx
@@ -1,7 +1,9 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { SplashScreen } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import ProfileHeader from '../../components/Headers/profileHeader';
+const userPlaceholder = require('../../assets/images/user-round.png');
+const squarePen = require('../../assets/images/square-pen.png');
 
 SplashScreen.preventAutoHideAsync();
 
@@ -16,10 +18,49 @@ export default function HomeScreen() {
       >
         {/* MAIN CONTENT */}
         <ProfileHeader />
+        <View style={styles.profileContainer}>
+          <Image source={userPlaceholder} style={styles.userImage}></Image>
+          <View>
+            <Text style={styles.username}>[@username]</Text>
+            <Text style={styles.email}>[email@domain.com]]</Text>
+          </View>
+          <View>
+            <Image source={squarePen} style={styles.editIcon}></Image>
+          </View>
+        </View>
       </LinearGradient>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  userImage: {
+    width: 70,
+    height: 70,
+    marginVertical: 5,
+    marginRight: 5,
+  },
+  profileContainer: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems:'center',
+    flexDirection: 'row',
+    paddingTop: 20,
+    paddingLeft: 20,
+  },
+  username: {
+    color: 'white',
+    fontFamily: 'Nature-Regular',
+    fontSize: 18,
+  },
+  email: {
+    color: 'white',
+    fontFamily: 'Nature-Light',
+    fontSize: 14,
+  },
+  editIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 100,
+  }
 });
